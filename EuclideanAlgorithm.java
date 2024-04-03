65dd2b089e34db249f7eedaae5eedf525aa5ccaf
kalink-32 <kaleabbayeh@gmail.com>
2024-04-04 00:33:52 +0300

    modified enclideanalgorithm by kalink-32

--- EuclideanAlgorithm.java
+++ EuclideanAlgorithm.java
@@ -4,25 +4,12 @@ import java.util.Scanner;
 
 public class EuclideanAlgorithm {
 
-    public int findGCD(int a, int b) {
-        while (b != 0) {
-            int temp = b;
-            b = a % b;
-            a = temp;
+    public static int gcd(int a, int b) {
+        if (b == 0) {
+            return a;
+        } else {
+            return gcd(b, a % b);
         }
-        return a;
-    }
-
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter the first number: ");
-        int num1 = scanner.nextInt();
-        System.out.print("Enter the second number: ");
-        int num2 = scanner.nextInt();
-
-        EuclideanAlgorithm euclideanAlgorithm = new EuclideanAlgorithm();
-        int gcd = euclideanAlgorithm.findGCD(num1, num2);
-        System.out.println("The Greatest Common Divisor (GCD) of " + num1 + " and " + num2 + " is: " + gcd);
     }
 }
 
