2233f6fed40b267312aeebd465001e2600493fe9
kalink-32 <kaleabbayeh@gmail.com>
2024-04-04 00:13:13 +0300

    commit my assignment Euclidean Algorithm

---
+++ MathUtils.java
@@ -0,0 +1,11 @@
+package kproject
+
+public class MathUtils {
+    public static int gcd(int a, int b) {
+        if (b == 0) {
+            return a;
+        } else {
+            return gcd(b, a % b);
+        }
+    }
+}
