4f94a94d53df11d0810c5d6e8633d8a03204f20d
abebe55 <abebemarye5360@gmail.com>
2024-04-04 15:53:20 +0300

    i commit the modified assignment that is the correct one

--- DecimalNumberReversal.java
+++ DecimalNumberReversal.java
@@ -1,25 +1,47 @@
 package toolsAssignment;
 
+
 import java.util.Scanner;
 
 public class DecimalNumberReversal {
 
-    public String reverseDecimalNumber(double number) {
-        String numStr = Double.toString(number);
-        String[] parts = numStr.split("\\.");
-        String integerPart = new StringBuilder(parts[0]).reverse().toString();
-        String decimalPart = new StringBuilder(parts[1]).reverse().toString();
-        return decimalPart + "." + integerPart;
-    }
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter a decimal number: ");
-        double inputNumber = scanner.nextDouble();
+        double number = scanner.nextDouble();
+        System.out.println("Reversed number: " + reverseDecimalNumber(number));
+        scanner.close();
+    }
 
-        DecimalNumberReversal decimalNumberReversal = new DecimalNumberReversal();
-        String reversedNumber = decimalNumberReversal.reverseDecimalNumber(inputNumber);
-        System.out.println("Reversed decimal number: " + reversedNumber);
+    public static double reverseDecimalNumber(double number) {
+        String numberStr = String.valueOf(number);
+        int dotpos=numberStr.indexOf(".");
+        StringBuilder reversedStr = new StringBuilder(numberStr.replace(".","" ));
+
+
+        StringBuilder decimalPart = reversedStr.reverse();
+
+        StringBuilder  decimalPart2=new StringBuilder();
+        int d=0;
+      for(int i=0;i<=decimalPart.length();i++) {
+    	  if(i==dotpos) {
+
+    		  decimalPart2.append(".");
+    		  d=1;
+
+    	  }
+    	  else if(d==1) {
+
+
+    		  decimalPart2.append(decimalPart.charAt(i-1));
+    	  }
+    	  else
+
+    		  decimalPart2.append(decimalPart.charAt(i));
+      }
+
+
+
+        return Double.parseDouble(decimalPart2.toString());
     }
 }
-
--- DecimalNumberReversalTest.java
+++ DecimalNumberReversalTest.java
@@ -1,26 +1,15 @@
 package toolsAssignment;
 
-
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DecimalNumberReversalTest {
 
     @Test
     public void testReverseDecimalNumber() {
-        DecimalNumberReversal decimalNumberReversal = new DecimalNumberReversal();
-        assertEquals("65.391", decimalNumberReversal.reverseDecimalNumber(193.56));
+        assertEquals(653.91, DecimalNumberReversal.reverseDecimalNumber(193.56));
+        assertEquals(876.54321, DecimalNumberReversal.reverseDecimalNumber(123.45678));
+        assertEquals(8.764, DecimalNumberReversal.reverseDecimalNumber(4.678));
     }
 
-    @Test
-    public void testReverseDecimalNumberWithZero() {
-        DecimalNumberReversal decimalNumberReversal = new DecimalNumberReversal();
-        assertEquals("0.0", decimalNumberReversal.reverseDecimalNumber(0.0));
-    }
-
-    @Test
-    public void testReverseDecimalNumberWithNegative() {
-        DecimalNumberReversal decimalNumberReversal = new DecimalNumberReversal();
-        assertEquals("65.391-", decimalNumberReversal.reverseDecimalNumber(-193.56));
-    }
 }
